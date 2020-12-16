a2adcebffeabdc96eaaa3f17ed884ddc5d52bfbf
HRITHIK <hrithikrtiwari@gmail.com>
2020-12-16 19:35:51 +0530

    added firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBZEAzgPFpxTb1RRFEXu3OC6NE5uBSBdug",
+  authDomain: "web-dux.firebaseapp.com",
+  databaseURL: "https://web-dux.firebaseio.com",
+  projectId: "web-dux",
+  storageBucket: "web-dux.appspot.com",
+  messagingSenderId: "336380058838",
+  appId: "1:336380058838:web:bee7b05f4a25eb8c94bd77",
+  measurementId: "G-1KMJ7B9EGG",
+};
