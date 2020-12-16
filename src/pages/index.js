8ae445fa0fcb50747ffeee9bca7042aa7e33f502
HRITHIK <hrithikrtiwari@gmail.com>
2020-12-17 00:00:57 +0530

    landing layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,42 @@
-import * as React from "react"
+import * as React from "react";
 
 // styles
 const pageStyles = {
   color: "#232129",
   padding: "96px",
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+};
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 320,
-}
+};
 const headingAccentStyles = {
   color: "#663399",
-}
+};
 const paragraphStyles = {
   marginBottom: 48,
-}
+};
 const codeStyles = {
   color: "#8A6534",
   padding: 4,
   backgroundColor: "#FFF4DB",
   fontSize: "1.25rem",
   borderRadius: 4,
-}
+};
 const listStyles = {
   marginBottom: 96,
   paddingLeft: 0,
   listStyleType: "none",
-}
+};
 const listItemStyles = {
   marginBottom: 12,
   fontWeight: "300",
   letterSpacing: 1,
-}
+};
 const linkStyles = {
   color: "#8954A8",
-}
+};
 
 // data
 const links = [
@@ -64,46 +64,102 @@ const links = [
     text: "Cheat Sheet",
     url: "https://www.gatsbyjs.com/docs/cheat-sheet/",
   },
-]
+];
 
 // markup
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site!</span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li style={listItemStyles}>
-            <a
-              style={linkStyles}
-              href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-            >
-              {link.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+      <div class="container-fluid d-flex flex-column justify-content-center align-items-center vh-100 banner">
+        <h1 class="font-size-400 satisfy-font text-blue">WebDux</h1>
+        <h3 class="langar-font">An Opportunity to be what you can be</h3>
+      </div>
+      <div class="container vh-100 w-100 row">
+        <div class="col-md-4 justify-content-center align-content-center">
+          <img
+            class="w-100"
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.DEZdJ_76NuiMoUexnMP3IAHaG0%26pid%3DApi&amp;f=1"
+            alt="Our Location"
+          />
+        </div>
+        <div class="col-md-8 d-flex justify-content-center align-content-center text-center">
+          <h1 class="langar-font">
+            Near the Financial, Commercial, and the Entertainment capital of
+            India
+          </h1>
+        </div>
+      </div>
+      <div class="container vh-100 w-100">
+        <h1>We build success successfully for you</h1>
+        <div class="container-fluid">
+          <div class="container">
+            <h4>Designing</h4>WITH CREATIVITY
+          </div>
+          <div class="container">
+            <h4>Programming </h4>WITH DEDICATION
+          </div>
+          <div class="container">
+            <h4>Testing </h4>WITH EXPERTISE
+          </div>
+          <div class="container">
+            <h4>Deploying </h4>WITH CONFIDENCE
+          </div>
+        </div>
+      </div>
+      <div class="container vh-100 w-100">
+        <h1>
+          We are collectives of
+          <div class="container">
+            <h3>Creative Writers</h3>
+          </div>
+          <div class="container">
+            <h3>Web Designers</h3>
+          </div>
+          <div class="container">
+            <h3>Graphic Designers</h3>
+          </div>
+          <div class="container">
+            <h3>Software Developers</h3>
+          </div>
+          <div class="container">
+            <h3>Android, iOS Developers</h3>
+          </div>
+          <div class="container">
+            <h3>Web Developers</h3>
+          </div>
+          <div class="container">
+            <h3>IoT Personnel </h3>
+          </div>
+          <div class="container">
+            <h3>System Designers</h3>
+          </div>
+          <div class="container">
+            <h3>UML Creators</h3>
+          </div>
+          <div class="container">
+            <h3>Game Designers</h3>
+          </div>
+        </h1>
+      </div>
+      <div class="container vh-100 w-100">
+        <h1>We Venture for - The Dedicated</h1>
+        <div class="row">
+          <div class="col-md-3">#ModernInspiredByAncient</div>
+          <div class="col-md-3">#StartupForGood</div>
+          <div class="col-md-3">#DigitalForNonProfit</div>
+          <div class="col-md-3">#PromotingWellBeing</div>
+        </div>
+      </div>
+      <div class="container vh-100 w-100">
+        <h1>Break the brick rather lying in the Inbox</h1>
+        <h2>A brand is what drives success</h2>
+        <div class="container">
+          <h1>How to contact WebDux</h1>
+        </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
