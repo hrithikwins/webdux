7fdc34e4efd35cdf58df64bfa1c93254980c5ab2
HRITHIK <hrithikrtiwari@gmail.com>
2020-12-17 00:00:03 +0530

    404 changes

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,28 +1,28 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import { Link } from "gatsby";
 
 // styles
 const pageStyles = {
   color: "#232129",
   padding: "96px",
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+};
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 320,
-}
+};
 
 const paragraphStyles = {
   marginBottom: 48,
-}
+};
 const codeStyles = {
   color: "#8A6534",
   padding: 4,
   backgroundColor: "#FFF4DB",
   fontSize: "1.25rem",
   borderRadius: 4,
-}
+};
 
 // markup
 const IndexPage = () => {
@@ -35,20 +35,29 @@ const IndexPage = () => {
         <span role="img" aria-label="Pensive emoji">
           😔
         </span>{" "}
-        we couldn’t find what you were looking for. 
-        <br/>
+        we couldn’t find what you were looking for.
+        <br />
         {process.env.NODE_ENV === "development" ? (
           <>
-            <br/>
-              Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br/>
+            <br />
+            Try checking the website url you have entered or go back to{" "}
+            <a href="https://webdux.tech">
+              <code style={codeStyles}>www.webdux.tech</code>
+            </a>
+            .
+            <br />
           </>
         ) : null}
-      <br/>
-        <Link to="/">Go home</Link>.
+        <br />
+        <Link to="/">Home</Link>
+        <Link to="/">Ventures</Link>
+        <Link to="/">Clients</Link>
+        <Link to="/">Works</Link>
+        <Link to="/">Team</Link>
+        <Link to="/">Technical Environment</Link>
       </p>
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
